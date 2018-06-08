a2a2082e0b672e962a5d4ff6d0108ecbf9d6a333
Maciek Kurek <maciejk77@gmail.com>
2018-06-08 14:38:51 +0000

    App start, dropdown and basic component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
 import Player from './components/player.js';
 
 class App extends Component {
--- src/components/player.js
+++ src/components/player.js
@@ -1,23 +1,59 @@
 import React, { Component }  from 'react';
 import '../css/styles.css';
 import no_player from '../assets/no_player.png';
+import player_data from '../data/player_stats.json';
 
-function Player() {
-	return (
-		<div className="block">
-			
-			<div className="drop-down">DROPDOWN</div>
-			
-			<div className="top-box">
-				<img className="image" src={no_player} />
-			</div>
-			
-			<div className="bottom-box">
-				STATS TEXT HERE!
-			</div>
-		
-		</div>
-	)
+class Player extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			value: '--- Select player ---'
+		}
+
+		this.handle_change = this.handle_change.bind(this)
+	}
+
+	handle_change(event) {
+		this.setState({ value: event.target.value });
+		console.log(event.target.value)
+	}
+
+	render() {
+
+    let player_info = player_data.players
+
+    let player_list = player_info.map(el => {
+      return <option key={el.player.id}>{el.player.name.last}</option>
+    })
+
+
+
+    return (
+
+      <div className="block">
+
+        <div className="drop-down">
+          <select onChange={this.handle_change} value={this.state.value}>
+            <option key={0}>-- Choose player ---</option>
+            {player_list}
+          </select>
+        </div>
+
+        <div className="top-box">
+          <img className="image" src={no_player} />
+        </div>
+
+        <div className="bottom-box">
+          STATS TEXT HERE!
+        </div>
+
+      </div>
+    )
+
+
+	}
 }
 
 export default Player
